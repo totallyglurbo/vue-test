32e880466dc26c676c88b3fa6c4e914b94c6575c
totallyglurbo <glurbin@glurb.com>
2026-02-16 16:29:03 +0700

    Change images Began changing how images are displayed

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: "Vue Mastery",
         description: "A pair of warm, fuzzy socks.",
         image: "./assets/vmSocks-blue-onWhite.jpg",
+        selectedVariant: 0,
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
@@ -27,13 +29,19 @@ let app = new Vue({
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.cart += 1;
         },
         deleteFromCart() {
-            this.cart -= 1
+            this.cart -= 1;
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
         }
     }
 })
