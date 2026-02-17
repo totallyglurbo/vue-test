b4670dbbf70802987733d4a6e5668c1694ad1cfe
totallyglurbo <glurb@glurbin.com>
2026-02-17 16:21:57 +0700

    Add review system Added a new component for product review, form for the review and displayed errors

--- js/main.js
+++ js/main.js
@@ -10,6 +10,75 @@ Vue.component('product-details',{
         }
     }
 })
+Vue.component('product-review',{
+    template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+        <p v-if="errors.length">
+            <b>Please correct the following error(s):</b>
+            <ul>
+                <li v-for="error in errors">{{ error }}</li>
+            </ul>
+        </p>
+        <p>
+            <label for="name">Name:</label>
+            <input id="name" v-model="name" placeholder="name">
+        </p>
+        <p>
+            <label for="review">Review:</label>
+            <textarea id="review" v-model="review"></textarea>
+        </p>
+        <p>
+            <label for="rating">Rating:</label>
+            <select id="rating" v-model.number="rating">
+                <option>5</option>
+                <option>4</option>
+                <option>3</option>
+                <option>2</option>
+                <option>1</option>
+            </select>
+        </p>
+        <p>Would you recommend this product?</p>
+        <label for="yes">Yes</label>
+        <input type="radio" value="Yes" id="yes" name="recommend" v-model.number="recommend">
+        <label for="no">No</label>
+        <input type="radio" value="No" id="no" name="recommend" v-model.number="recommend">
+        <p>
+            <input type="submit" value="Submit">
+        </p>
+    </form>
+    `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            recommend: null,
+            errors: []
+        }
+    },
+    methods: {
+        onSubmit() {
+            if(this.name && this.review && this.rating && this.recommend) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    recommend: this.recommend,
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+                this.recommend = null
+            } else {
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+                if(!this.recommend) this.errors.push("Recommendation required.")
+            }
+        },
+    }
+})
 Vue.component('product', {
     props: {
         premium: {
@@ -52,6 +121,19 @@ Vue.component('product', {
             >
                 Add to cart</button>
             <button v-on:click="deleteFromCart">Delete from cart</button>
+            <div>
+            <h2>Reviews</h2>
+            <p v-if="!reviews.length">There are no reviews yet.</p>
+            <ul>
+              <li v-for="review in reviews">
+              <p>{{ review.name }}</p>
+              <p>Rating: {{ review.rating }}</p>
+              <p>{{ review.review }}</p>
+              <p>Would recommend: {{review.recommend}}</p>
+              </li>
+            </ul>
+        </div>
+        <product-review @review-submitted="addReview"></product-review>
         </div>
     </div>
     `,
@@ -79,6 +161,7 @@ Vue.component('product', {
                 }
             ],
             sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            reviews: []
         }
     },
     methods: {
@@ -93,6 +176,9 @@ Vue.component('product', {
         updateProduct(index) {
             this.selectedVariant = index;
             console.log(index);
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
         }
     },
     computed: {
