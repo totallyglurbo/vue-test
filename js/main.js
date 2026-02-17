57ca68c18b439f238dd9fbabdc3d2a5ae974e3eb
totallyglurbo <glurb@glurbin.com>
2026-02-17 14:43:37 +0700

    Add a component for product details Added a line to display shipping cost, added premium and moved product details into a separate component

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
+Vue.component('product-details',{
+    template: `
+    <ul>
+       <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+    `,
+    data() {
+        return {
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        }
+    }
+})
 Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
     template: `
     <div class="product">
     <div class="product-image">
@@ -11,9 +29,8 @@ Vue.component('product', {
             <p v-if="variants[selectedVariant].variantQuantity > 10 && inStock">In Stock</p>
             <p v-else-if="variants[selectedVariant].variantQuantity <= 10 && variants[selectedVariant].variantQuantity > 0 && inStock">Almost sold out!</p>
             <p v-else :class="{ outOfStock: !inStock || variants[selectedVariant].variantQuantity <= 0 }">Out of Stock</p>
-            <ul>
-                <li v-for="detail in details">{{ detail }}</li>
-            </ul>
+            <product-details></product-details>
+            <p>Shipping: {{ shipping }}</p>
             <div
                     class="color-box"
                     v-for="(variant, index) in variants"
@@ -50,7 +67,6 @@ Vue.component('product', {
             altText: "A pair of socks",
             link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
             onSale: true,
-            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
             variants: [
                 {
                     variantId: 2234,
@@ -98,9 +114,20 @@ Vue.component('product', {
             else {
                 return this.brand + ' ' + this.product + ' are not on sale.';
             }
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free";
+            }
+            else {
+                return 2.99;
+            }
         }
     }
 })
 let app = new Vue({
     el: '#app',
+    data: {
+        premium: true
+    }
 })
