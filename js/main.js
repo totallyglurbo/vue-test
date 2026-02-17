a521a42739dbbe665a220d1dc802c9409f556480
totallyglurbo <glurb@glurbin.com>
2026-02-17 12:39:24 +0700

    Add computed properties Changed how the sale line is being displayed, deleted inventory and added variant quantity instead

--- js/main.js
+++ js/main.js
@@ -4,12 +4,9 @@ let app = new Vue({
         product: "Socks",
         brand: "Vue Mastery",
         description: "A pair of warm, fuzzy socks.",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
         selectedVariant: 0,
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true,
-        inventory: 100,
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
@@ -17,11 +14,13 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: './assets/vmSocks-green-onWhite.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: './assets/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -42,6 +41,20 @@ let app = new Vue({
     computed: {
         title() {
             return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.onSale && this.inStock) {
+                return this.brand + ' ' + this.product + ' are on sale!';
+            }
+            else {
+                return this.brand + ' ' + this.product + ' are not on sale.';
+            }
         }
     }
 })
